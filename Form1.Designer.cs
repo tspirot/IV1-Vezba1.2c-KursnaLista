d84b35212824b7922825ef18e6cef296f300c29f
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2023-09-26 23:12:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Vezba1._2c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxValuta = new System.Windows.Forms.TextBox();
+            this.textBoxKurs = new System.Windows.Forms.TextBox();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonObrisi = new System.Windows.Forms.Button();
+            this.listBoxKursevi = new System.Windows.Forms.ListBox();
+            this.buttonIzmeni = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valuta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kurs";
+            // 
+            // textBoxValuta
+            // 
+            this.textBoxValuta.Location = new System.Drawing.Point(132, 35);
+            this.textBoxValuta.Name = "textBoxValuta";
+            this.textBoxValuta.Size = new System.Drawing.Size(126, 22);
+            this.textBoxValuta.TabIndex = 2;
+            // 
+            // textBoxKurs
+            // 
+            this.textBoxKurs.Location = new System.Drawing.Point(132, 69);
+            this.textBoxKurs.Name = "textBoxKurs";
+            this.textBoxKurs.Size = new System.Drawing.Size(126, 22);
+            this.textBoxKurs.TabIndex = 3;
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(51, 116);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(75, 23);
+            this.buttonDodaj.TabIndex = 4;
+            this.buttonDodaj.Text = "Dodaj";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // buttonObrisi
+            // 
+            this.buttonObrisi.Location = new System.Drawing.Point(132, 116);
+            this.buttonObrisi.Name = "buttonObrisi";
+            this.buttonObrisi.Size = new System.Drawing.Size(75, 23);
+            this.buttonObrisi.TabIndex = 5;
+            this.buttonObrisi.Text = "Obriši";
+            this.buttonObrisi.UseVisualStyleBackColor = true;
+            this.buttonObrisi.Click += new System.EventHandler(this.buttonObrisi_Click);
+            // 
+            // listBoxKursevi
+            // 
+            this.listBoxKursevi.FormattingEnabled = true;
+            this.listBoxKursevi.ItemHeight = 16;
+            this.listBoxKursevi.Location = new System.Drawing.Point(50, 163);
+            this.listBoxKursevi.Name = "listBoxKursevi";
+            this.listBoxKursevi.Size = new System.Drawing.Size(208, 276);
+            this.listBoxKursevi.TabIndex = 6;
+            this.listBoxKursevi.SelectedIndexChanged += new System.EventHandler(this.listBoxKursevi_SelectedIndexChanged);
+            // 
+            // buttonIzmeni
+            // 
+            this.buttonIzmeni.Location = new System.Drawing.Point(213, 116);
+            this.buttonIzmeni.Name = "buttonIzmeni";
+            this.buttonIzmeni.Size = new System.Drawing.Size(75, 23);
+            this.buttonIzmeni.TabIndex = 7;
+            this.buttonIzmeni.Text = "Izmeni";
+            this.buttonIzmeni.UseVisualStyleBackColor = true;
+            this.buttonIzmeni.Click += new System.EventHandler(this.buttonIzmeni_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 450);
+            this.Controls.Add(this.buttonIzmeni);
+            this.Controls.Add(this.listBoxKursevi);
+            this.Controls.Add(this.buttonObrisi);
+            this.Controls.Add(this.buttonDodaj);
+            this.Controls.Add(this.textBoxKurs);
+            this.Controls.Add(this.textBoxValuta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kursna lista";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxValuta;
+        private System.Windows.Forms.TextBox textBoxKurs;
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonObrisi;
+        private System.Windows.Forms.ListBox listBoxKursevi;
+        private System.Windows.Forms.Button buttonIzmeni;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vezba1._2c
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> kursevi = 
+            new Dictionary<string, double>();
+        BindingSource bs;
+        public Form1()
+        {
+            InitializeComponent();
+            //listBoxKursevi.Items.Add("USD");
+        }
+        // primer zadatka za genericku metodu
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bs = new BindingSource(kursevi, null);
+            listBoxKursevi.DataSource = bs;
+            listBoxKursevi.DisplayMember = "Key";
+            kursevi.Add("EUR",117.5);
+            OsveziListBox();
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            kursevi.Add(textBoxValuta.Text,
+                double.Parse(textBoxKurs.Text));
+            OsveziListBox();
+        }
+        public void OsveziListBox()
+        {
+            listBoxKursevi.Items.Clear();
+            foreach (var item in kursevi)
+            {
+                listBoxKursevi.Items.Add(item);
+            }
+        }
+
+        private void buttonObrisi_Click(object sender, EventArgs e)
+        {
+            if(listBoxKursevi.SelectedIndex == -1)
+            {
+                MessageBox.Show("Niste selektovali valutu");
+                return;
+            }
+            string kljuc;
+            kljuc = ((KeyValuePair<string, double>)listBoxKursevi.SelectedItem).Key;
+            kursevi.Remove(kljuc);
+            OsveziListBox();
+        }
+
+        private void listBoxKursevi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxKursevi.SelectedIndex != -1)//ako je selektovano
+            {
+                textBoxValuta.Text = ((KeyValuePair<string, double>)
+                    listBoxKursevi.SelectedItem).Key;
+                textBoxKurs.Text = ((KeyValuePair<string, double>)
+                    listBoxKursevi.SelectedItem).Value.ToString();
+            }
+            else //ako nije selektovano
+            {
+                textBoxValuta.Text = "";
+                textBoxKurs.Text = "";
+            }
+        }
+        private void buttonIzmeni_Click(object sender, EventArgs e)
+        {
+            if(listBoxKursevi.SelectedIndex == -1)
+                MessageBox.Show("Niste selektovali valutu");
+            else
+            {
+                string kljuc;
+                kljuc = ((KeyValuePair<string, double>)
+                                 listBoxKursevi.SelectedItem).Key;
+                kursevi[kljuc] = double.Parse(textBoxKurs.Text);
+                OsveziListBox();
+            }
+        }
+    }
+}
